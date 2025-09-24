7335c246e1d862126f607e6651e84c0d0303af51
Hussain Haider <ihussainhaider1@gmail.com>
2025-09-24 19:58:07 +0500

    complete single page portfolio update

--- port.js
+++ port.js
@@ -10,10 +10,4 @@ window.onclick = function(event) {
   });
 }
 
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
 
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('active');  // show/hide menu
-  hamburger.classList.toggle('active'); // animate hamburger
-});
